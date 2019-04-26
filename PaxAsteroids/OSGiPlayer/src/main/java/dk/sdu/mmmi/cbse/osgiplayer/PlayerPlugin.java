372508b8e3da46222f480067649d5bbba1488edc
Borgar Bordoy <bobor16@student.sdu.dk>
2019-04-26 09:55:15 +0200

    sup

---
+++ PaxAsteroids/OSGiPlayer/src/main/java/dk/sdu/mmmi/cbse/osgiplayer/PlayerPlugin.java
@@ -0,0 +1,51 @@
+package dk.sdu.mmmi.cbse.osgiplayer;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.player.Player;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+
+public class PlayerPlugin implements IGamePluginService {
+    private String playerID;
+
+    public PlayerPlugin() {
+    }
+
+    @Override
+    public void start(GameData gameData, World world) {
+        // Add entities to the world
+        Entity player = createPlayerShip(gameData);
+        playerID = world.addEntity(player);
+        
+    }
+
+    private Entity createPlayerShip(GameData gameData) {
+        Entity enemyShip = new Player();
+
+        float deacceleration = 10;
+        float acceleration = 200;
+        float maxSpeed = 300;
+        float rotationSpeed = 5;
+        float x = gameData.getDisplayWidth() / 3;
+        float y = gameData.getDisplayHeight() / 3;
+        float radians = 3.1415f / 2;
+        enemyShip.add(new LifePart(3));
+        enemyShip.setRadius(4);
+        enemyShip.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+        enemyShip.add(new PositionPart(x, y, radians));
+        
+        return enemyShip;
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        // Remove entities
+        world.removeEntity(playerID);
+    }
+
+}
