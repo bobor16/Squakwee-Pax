c73219fdff32e82ade32bdfd9b517dd25e9dd636
Borgar Bordoy <bobor16@student.sdu.dk>
2019-05-26 20:53:45 +0200

    ss

---
+++ Squakwee/OSGiMap/target/classes/maps/Tiles.tsx
@@ -0,0 +1,298 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tileset" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="435" columns="29">
+ <image source="Tiles.png" width="502" height="263"/>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" name="blocked" x="0.197658" y="0" width="15.7138" height="16.0103">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0" width="15.75" height="16">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="174">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="7.02755" y="7.23424" width="8.88779" height="8.4744">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="175">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="3.16252" width="15.8126" height="12.5513">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+   <object id="2" x="0" y="3.30708" width="15.502" height="12.8149">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="176">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="2" x="7.23424" y="9.09448" width="8.68109" height="6.82086">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+   <object id="3" x="0" y="1.24016" width="7.02755" height="15.0886">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+   <object id="4" x="7.44094" y="4.75393" width="5.16732" height="4.13385">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="177">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="203">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="8.26771" y="0.206693" width="7.85432" height="15.502">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+   <object id="2" x="0.620078" y="7.85432" width="7.85432" height="8.26771">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="204">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0.620078" y="0.206693" width="15.0886" height="15.7086">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="205">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0.413385" y="0" width="15.0886" height="15.7086">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="206">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.620078" y="0" width="9.30117" height="15.7086">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="232">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="7.64763" y="0.206693" width="7.85432" height="15.0886">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+   <object id="3" x="2.27362" y="0.206693" width="5.5807" height="15.2953">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="233">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0.413385" y="0" width="15.7086" height="16.122">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="234">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="15.7086" height="16.122">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="235">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.206693" y="0.206693" width="10.3346" height="15.7086">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="261">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="262">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.206693" y="-0.826771" width="16.122" height="9.09448">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+   <object id="2" x="7.02755" y="8.68109" width="9.09448" height="7.23424">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="263">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16.122" height="7.85432">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="264">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="290">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="291">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="8.06102" y="0.826771" width="7.64763" height="14.6752">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+   <object id="2" x="2.8937" y="4.75393" width="5.37401" height="8.88779">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="292">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.413385" width="4.96063" height="15.9153">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+   <object id="2" x="5.16732" y="5.5807" width="3.92716" height="10.5413">
+    <properties>
+     <property name="blocked" value=""/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="293">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="319">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="320">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="321">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="322">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="379">
+  <animation>
+   <frame tileid="366" duration="100"/>
+   <frame tileid="367" duration="100"/>
+   <frame tileid="368" duration="100"/>
+   <frame tileid="369" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
